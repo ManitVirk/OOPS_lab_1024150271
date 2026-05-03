c43107eee1b95509071bd5d769dd62abcc369082
ManitVirk <3301manit@gmail.com>
2026-05-03 08:57:39 +0530

    Create QUESTION5.cpp

---
+++ assignment1_1024150271/QUESTION5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "one\n";
+    cout << "two\n";
+    cout << "1\t2\t3" << endl;
+    cout << "xyz \bq" << endl;
+    cout << "line one" << endl;
+    return 0;
+}
